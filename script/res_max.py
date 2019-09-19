6680deaa20231fc3826328808f2de903b2351345
namickey <teethdai@gmail.com>
2019-09-19 15:51:02 +0900

    Create res_max.py

---
+++ script/res_max.py
@@ -0,0 +1,18 @@
+import pandas as pd
+%matplotlib inline
+import matplotlib.pyplot as plt
+fig, (a1) = plt.subplots(ncols=1, figsize=(25,8), sharey=True)
+fig.suptitle(u'最大レスポンス 1秒間隔')
+def graph(a, path, title):
+    df = pd.read_csv(path)
+    df['a'] = (df['a']/1000).round().astype(int)
+    gf = df[['a','b']]
+    gfed = gf.groupby('a').max()
+    a.set_title(title)
+    a.set_ylim([0, 10000])
+    a.set_yticks(range(0,10000,1000))
+    a.grid(True)
+    a.plot(gfed.index, gfed['b'])
+a1.set_ylabel(u'レスポンス ミリ秒')
+graph(a1, 'log.csv', u'max ave')
+fig.savefig('max.png')
